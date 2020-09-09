035504feff2da10abe8b0a56f97d35cfc9f8d76f
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 18:51:43 +0545

    added the product

---
+++ src/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css';
+
+function Product(){
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>This is the product description</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>&#127775;</p>
+                    <p>&#127775;</p>
+                    <p>&#127775;</p>
+                </div>
+            </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg" alt=""/>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product;
