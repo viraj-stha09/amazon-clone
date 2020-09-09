a24c97f833990887ab15dad5135f25f3c821236f
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 18:01:43 +0545

    Initial commit with header

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header(){
+    return (
+        <div className="header">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png " alt=""/>
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
+             </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Viraj</span>
+                    <span className="header__optionLineOne">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineOne">Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineOne">Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNq20lW62i3yep_vdF7rZfyRDFkkNLi_A",
+    authDomain: "clone-4c1aa.firebaseapp.com",
+    databaseURL: "https://clone-4c1aa.firebaseio.com",
+    projectId: "clone-4c1aa",
+    storageBucket: "clone-4c1aa.appspot.com",
+    messagingSenderId: "1026159441594",
+    appId: "1:1026159441594:web:ecb872d90a79fc20e53483"
+};
