8867be783d56c8afbcdaf17031d3089d306466af
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 18:24:50 +0545

    header completed

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header(){
     return (
@@ -13,15 +14,19 @@ function Header(){
             <div className="header__nav">
                 <div className="header__option">
                     <span className="header__optionLineOne">Hello Viraj</span>
-                    <span className="header__optionLineOne">Sign In</span>
+                    <span className="header__optionLineTwo">Sign In</span>
                 </div>
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
-                    <span className="header__optionLineOne">Orders</span>
+                    <span className="header__optionLineTwo">Orders</span>
                 </div>
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
-                    <span className="header__optionLineOne">Prime</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineOne header_basketCount">0</span>
                 </div>
             </div>
         </div>
