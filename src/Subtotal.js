4fcd185a9be2a476ca439853370300b4f169b5e8
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 20:43:51 +0545

    added the subtotal component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Subtotal.css';
+import CurrencyFprmat from "react-currency-format";
 
 function Subtotal(){
     return (
         <div className="subtotal">
-
+            <CurrencyFprmat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className="subtotal_gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
