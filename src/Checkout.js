088e95db22c51b73f3f4c39127ccab24071691d2
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 20:15:00 +0545

    added the checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,19 @@ import './Checkout.css';
 function Checkout(){
     return (
         <div className="checkout">
-
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
+                <div>
+                    <h2 className="checkout__title" >Your Shopping Basket</h2>
+                    {/*Basket item*/}
+                    {/*Basket item*/}
+                    {/*Basket item*/}
+                    {/*Basket item*/}
+                </div>
+            </div>
+            <div className="checkout__right">
+                <h2>Subtotal go here</h2>
+            </div>
         </div>
     )
 }
