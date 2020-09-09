1864be5d5ab3452dfada88cc826bab4188fd589e
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-09 20:12:54 +0545

    added the link to home and checkout pages

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from "react-router-dom";
 
 function Header(){
     return (
         <div className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png " alt=""/>
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png " alt=""/>
+            </Link>
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
                 <SearchIcon className="header__searchIcon" />
@@ -25,8 +28,10 @@ function Header(){
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
                 <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionLineOne header_basketCount">0</span>
+                    <Link to="/checkout">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineOne header_basketCount">0</span>
+                    </Link>
                 </div>
             </div>
         </div>
