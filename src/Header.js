2bde29122aca65adc3c681909c8e3876a1563853
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-10 22:05:30 +0545

    added the login page for amazon

--- src/Header.js
+++ src/Header.js
@@ -17,10 +17,13 @@ function Header(){
                 <SearchIcon className="header__searchIcon" />
              </div>
             <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Viraj</span>
-                    <span className="header__optionLineTwo">Sign In</span>
-                </div>
+                <Link to="/login">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Viraj</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns</span>
                     <span className="header__optionLineTwo">Orders</span>
