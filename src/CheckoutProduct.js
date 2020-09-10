58dd651051ef97412e8af9a976305258dd8f607d
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-10 21:33:10 +0545

    added the functionality for removing the product from cart/basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './CheckoutProduct.css';
+import {useStateValue} from "./StateProvider";
 
 
 function CheckoutProduct({ id, image, title, price, rating}){
+    const [{basket}, dispatch] = useStateValue();
+    const removeFromBasket = () => {
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
     return (
         <div className="checkoutProduct">
             <img src={image} alt="" className="checkoutProduct__image"/>
@@ -18,7 +26,7 @@ function CheckoutProduct({ id, image, title, price, rating}){
                             <span role="img">&#127775;</span>
                         ))}
                 </div>
-                <button>Remove From Basket</button>
+                <button onClick={removeFromBasket}>Remove From Basket</button>
             </div>
         </div>
     )
