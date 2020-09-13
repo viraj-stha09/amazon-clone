3175c08c742e71470c04d91f7a65b40e13f8c1d6
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-13 15:22:03 +0545

    added the backend dependency

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_oZpOJSEYf35T2kzLMaryQxaP00urqixnOW");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//APP
+// - App config
+
+// - Middlewares
+
+// - API routes
+
+// - Listen Command
