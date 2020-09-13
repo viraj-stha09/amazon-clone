a8588ebd8393dd24b3da4547167ff7c96cd75ec3
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-13 16:08:02 +0545

    added the back end to create the secret client key for the payment

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,25 @@ const stripe = require("stripe")("sk_test_oZpOJSEYf35T2kzLMaryQxaP00urqixnOW");
 
 //APP
 // - App config
-
+const app = express();
 // - Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
 
 // - API routes
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+    console.log("Payment Request Recieved BOOM??? for the amount >>> ", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+    });
+
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
 
 // - Listen Command
+exports.api = functions.https.onRequest(app)
