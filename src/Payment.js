6ef2ef1a0e8aa2e5630d191d531b834328e8805d
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-13 16:38:36 +0545

    added the functionality to store the purchase on the database

--- src/Payment.js
+++ src/Payment.js
@@ -7,6 +7,7 @@ import {useElements, useStripe, CardElement} from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from "./axios";
+import { db } from "./firebase";
 
 function Payment(){
     const [{basket,user}, dispatch] = useStateValue();
@@ -41,6 +42,17 @@ function Payment(){
                 card: elements.getElement(CardElement)
             }
         }).then(({paymentIntent}) => {
+
+            db.collection('users')
+                .doc(user?.uid)
+                .collection('orders')
+                .doc(paymentIntent.id)
+                .set({
+                    basket: basket,
+                    amount: paymentIntent.amount,
+                    created: paymentIntent.created
+                })
+
             setSucceeded(true);
             setError(null);
             setProcessing(false);
