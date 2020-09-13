7e95b2978e9355f2c526cf637aae61f374940a3f
Viraj Kumar Shrestha <viraj@studiomatrix.com.np>
2020-09-13 13:57:34 +0545

    added the stripe payment

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { auth } from "./firebase";
 import { useStateValue} from "./StateProvider";
+import { loadStripe} from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js"
+
+const promise = loadStripe("pk_test_79ZTY7ZABYRHTzGXoPCqPSVi00Pgpj779e");
 
 function App() {
     const [{}, dispatch] = useStateValue();
@@ -36,6 +41,12 @@ function App() {
                   <Route path="/checkout">
                       <Header/>
                       <Checkout />
+                  </Route>
+                  <Route path="/payment">
+                      <Header/>
+                      <Elements stripe={promise}>
+                          <Payment />
+                      </Elements>
                   </Route>
                   <Route path="/">
                       <Header/>
